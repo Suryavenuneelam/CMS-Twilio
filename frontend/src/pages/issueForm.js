6d8ab843fc01b9a00121121ce6bddd1271f93c67
Suryavenuneelam <sai.venkatasuryavenu2021@vitstudent.ac.in>
2024-07-27 16:17:28 +0530

    Updated Forms and UI

---
+++ frontend/src/pages/issueForm.js
@@ -0,0 +1,17 @@
+
+import AppAppBar from '../components/navbar';
+import IssueForm from '../components/issueForm';
+import SampleForm from '../components/sampleform';
+
+
+function Signinpage() {
+  return (
+    <div className="App">
+      <AppAppBar/>
+      <IssueForm />
+      {/* <SampleForm /> */}
+    </div>
+  );
+}
+
+export default Signinpage;
